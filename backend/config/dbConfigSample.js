c6f139d90a5030e722f1fab5e4eebe1e558d0231
solo95 <wengers1004@naver.com>
2020-07-10 17:58:23 +0900

    gitignore 변경

---
+++ backend/config/dbConfigSample.js
@@ -0,0 +1,9 @@
+const host = 'sample 128.0.0.1'
+module.exports ={
+    mysql:{
+        user     : 'sample id',
+        password : 'sample password',
+        host     : host,
+        database : 'sample db name',
+    }
+}
