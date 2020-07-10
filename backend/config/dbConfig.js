03e5d73a6ba96eb0ec5d15122a2b961dfebf3d6f
solo95 <wengers1004@naver.com>
2020-07-10 17:30:44 +0900

    gitignore 수정

--- backend/config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-const host = 'example, 128.115.115'
-module.exports ={
-    mysql:{
-        user     : 'id',
-        password : 'password',
-        host     : host,
-        database : 'db name',
-    }
-}
