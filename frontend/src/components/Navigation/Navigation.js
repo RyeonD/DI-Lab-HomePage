5f3e136192fb37df3500651de53e3ee856c1e5c9
WooByeongWook <wbo1026@naver.com>
2020-07-20 20:24:18 +0900

    회원가입 Modal로 띄우기 초본 (modal check! 버튼이 로그인화면 회원가입버튼으로 할예정)

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -13,68 +13,84 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import axios from 'axios';
+import Modal from '@material-ui/core/Modal'
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import SignUp from '../../page/SignUp/SignUp'
 import './styles.scss'
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
         position: 'relative'
     },
-    '@media screen and (max-width: 600px)' : {
-        navButton:{display:'none'},
-        menuButton:{display:'inline-flex'}
+    '@media screen and (max-width: 600px)': {
+        navButton: { display: 'none' },
+        menuButton: { display: 'inline-flex' }
     },
     navButton: {
         marginRight: theme.spacing(2),
     },
-    sideBarButton:{
-        width:'100%',
+    sideBarButton: {
+        width: '100%',
         marginRight: theme.spacing(2),
     },
-    '@media screen and (min-width: 601px)' : {
-        menuButton:{display:'none'}
+    '@media screen and (min-width: 601px)': {
+        menuButton: { display: 'none' }
     },
     title: {
         flexGrow: 1,
     },
-    sideBar:{
-        width:250
+    sideBar: {
+        width: 250
     },
-    noDecoration:{
-        color:'inherit',
-        textDecoration:'none',
-        '&::visited':{
-            color:'inherit',
-            textDecoration:'none',
+    noDecoration: {
+        color: 'inherit',
+        textDecoration: 'none',
+        '&::visited': {
+            color: 'inherit',
+            textDecoration: 'none',
         }
     },
-    
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+      },
+
 }));
 const StyledMenu = withStyles({
     paper: {
-        backgroundColor:'#3f51b5',
-        color:'#fff'
+        backgroundColor: '#3f51b5',
+        color: '#fff'
     }
-    
+
 })((props) => (
     <Menu elevation={0}
-          getContentAnchorEl={null}
-          anchorOrigin={{
-              vertical:'bottom',
-              horizontal:'center',
-          }}
-          transformOrigin={{
-              vertical:'top',
-              horizontal:'center'
-          }}
-          {...props}/>
+        getContentAnchorEl={null}
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+        }}
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center'
+        }}
+        {...props} />
 ));
 const MenuList = React.memo(({ sideBarFlag }) => {
     const classes = useStyles();
-    let   history = useHistory();
+    let history = useHistory();
     const [anchorEl, setAnchorEl] = useState(null);
     const [dropDownFlag, setDropDown] = useState(false);
+    const [open, setOpen] = React.useState(false);
     const openList = (e) => {
         setAnchorEl(e.currentTarget);
         setDropDown(!dropDownFlag)
@@ -87,104 +103,116 @@ const MenuList = React.memo(({ sideBarFlag }) => {
         setAnchorEl(null);
     }
     const callApi = () => {
-        axios.get('/api/user/userInfo',{
+        axios.get('/api/user/userInfo', {
             params: {
-                user_id:'201413286'
+                user_id: '201413286'
             }
-        }).then( res => {
+        }).then(res => {
             console.log(res);
         });
     };
+    
     const menuItems = [
-        { name: '연구실', 
-          destination: '' }, 
-        { name: '구성원' }, 
-        { name: '프로젝트'}, 
-        { name: '게시판', 
-          onClick: openMenu,
-          destination: 'openModal',
-          nested: [ 
-              { name: '강의 게시판',   
-                destination: 'forum/lecture'}, 
-              { name: '세미나 게시판', 
-                destination: 'forum/seminar'}]} , 
-        { name: 'Login',onClick:callApi },
-        { name: '회원가입',
-          destination: 'signup'},
+        {
+            name: '연구실',
+            destination: ''
+        },
+        { name: '구성원' },
+        { name: '프로젝트' },
+        {
+            name: '게시판',
+            onClick: openMenu,
+            destination: 'openModal',
+            nested: [
+                {
+                    name: '강의 게시판',
+                    destination: 'forum/lecture'
+                },
+                {
+                    name: '세미나 게시판',
+                    destination: 'forum/seminar'
+                }]
+        },
+        { name: 'Login', onClick: callApi },
+        { name: '회원가입', destination: 'signup' },
     ];
 
     return (
-        menuItems.map((item,i) => {
-            if (sideBarFlag && item.nested) { return (
-                <List key={i}>
-                    <Button className = {sideBarFlag ? classes.sideBarButton 
-                                                        : classes.navButton} 
-                            color     = "inherit"
-                            key       = {i}
-                            id        = {item.name}
-                            onClick   = {openList}>
-                        {item.name}
-                    </Button>
-                    <Collapse in      = {Boolean(dropDownFlag && anchorEl.id === item.name)} 
-                              timeout = "auto" 
-                              unmountOnExit>
-                        <List component = "div" 
+        menuItems.map((item, i) => {
+            if (sideBarFlag && item.nested) {
+                return (
+                    <List key={i}>
+                        <Button className={sideBarFlag ? classes.sideBarButton
+                            : classes.navButton}
+                            color="inherit"
+                            key={i}
+                            id={item.name}
+                            onClick={openList}>
+                            {item.name}
+                        </Button>
+                        <Collapse in={Boolean(dropDownFlag && anchorEl.id === item.name)}
+                            timeout="auto"
+                            unmountOnExit>
+                            <List component="div"
                                 disablePadding>
-                            {item.nested.map((nestedItem, j) => {
-                            return (
-                            <ListItem className = {classes.nested} 
-                                        key     = {`${i}-${j}`}
-                                        id      = {nestedItem.destination}
-                                        button
-                                        onClick = {nestedItem.onClick}>
-                                <NavLink className = {classes.noDecoration} to = {{
-                                    pathname:`/${nestedItem.destination}`
-                                }}>
-                                    <ListItemText primary={nestedItem.name} />
-                                </NavLink>
-                            </ListItem>)
-                            })}
-                        </List>
-                    </Collapse>
-                </List>
-            )}
+                                {item.nested.map((nestedItem, j) => {
+                                    return (
+                                        <ListItem className={classes.nested}
+                                            key={`${i}-${j}`}
+                                            id={nestedItem.destination}
+                                            button
+                                            onClick={nestedItem.onClick}>
+                                            <NavLink className={classes.noDecoration} to={{
+                                                pathname: `/${nestedItem.destination}`
+                                            }}>
+                                                <ListItemText primary={nestedItem.name} />
+                                            </NavLink>
+                                        </ListItem>)
+                                })}
+                            </List>
+                        </Collapse>
+                    </List>
+                )
+            }
             else {
                 return (
-                <React.Fragment key = {i}>
-                <Button className = {sideBarFlag ? classes.sideBarButton : classes.navButton} 
-                        color     = "inherit"
-                        key       = {i}
-                        id        = {item.destination}
-                        onClick   = {item.nested ? item.onClick : null}>
-                    {item.nested ? item.name :
-                    <NavLink className = {classes.noDecoration}to = {{
-                        pathname:`/${item.destination}`}}>
-                        {item.name}
-                    </NavLink>}
-                </Button>
-                {
-                item.nested ? 
-                <StyledMenu id         = {item.name}
-                            anchorEl   = {anchorEl}
-                            keepMounted
-                            open       = {Boolean(anchorEl && anchorEl.id === item.destination)}
-                            onClose    = {closeMenu}>
-                    {item.nested.map((nestedItem, j) => 
-                        <MenuItem key = {j}
-                                  id  = {nestedItem.destination}
-                                  onClick = {closeMenu}>
-                            <NavLink className = {classes.noDecoration}to = {{
-                                    pathname:`/${nestedItem.destination}`
-                            }}>
-                                {nestedItem.name}
-                            </NavLink>
-                        </MenuItem>
-                    )}
-                </StyledMenu>:null
-                }
-                </React.Fragment>
-            )}
-        })  
+                    <React.Fragment key={i}>
+                        <Button className={sideBarFlag ? classes.sideBarButton : classes.navButton}
+                            color="inherit"
+                            key={i}
+                            id={item.destination}
+                            onClick={item.nested ? item.onClick : null}>
+                            {item.nested ? item.name :
+                                <NavLink className={classes.noDecoration} to={{
+                                    pathname: `/${item.destination}`
+                                }}>
+                                    {item.name}
+                                </NavLink>}
+                        </Button>
+                        {
+                            item.nested ?
+                                <StyledMenu id={item.name}
+                                    anchorEl={anchorEl}
+                                    keepMounted
+                                    open={Boolean(anchorEl && anchorEl.id === item.destination)}
+                                    onClose={closeMenu}>
+                                    {item.nested.map((nestedItem, j) =>
+                                        <MenuItem key={j}
+                                            id={nestedItem.destination}
+                                            onClick={closeMenu}>
+                                            <NavLink className={classes.noDecoration} to={{
+                                                pathname: `/${nestedItem.destination}`
+                                            }}>
+                                                {nestedItem.name}
+                                            </NavLink>
+                                        </MenuItem>
+                                    )}
+                                </StyledMenu> : null
+                        }
+                    </React.Fragment>
+                )
+            }
+        })
     )
 })
 const Navigation = () => {
@@ -192,8 +220,8 @@ const Navigation = () => {
     const [toggleFlag, setToggleFlag] = useState(false)
     const toggleDrawer = useCallback(e => {
         setToggleFlag(!toggleFlag)
-    },[toggleFlag])
-    
+    }, [toggleFlag])
+
     return (
         <div className={classes.root}>
             <AppBar position="static">
@@ -201,19 +229,19 @@ const Navigation = () => {
                     <Typography variant="h6" className={classes.title}>
                         DI Lab
                     </Typography>
-                    <MenuList/>
-                    <IconButton edge       ="start" 
-                                className  = {classes.menuButton} 
-                                color      = "inherit" 
-                                aria-label = "menu" 
-                                onClick    = {toggleDrawer}>
+                    <MenuList />
+                    <IconButton edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={toggleDrawer}>
                         <MenuIcon />
                     </IconButton>
-                    <Drawer anchor  = 'right' 
-                            open    = {toggleFlag} 
-                            onClose = {toggleDrawer}>
+                    <Drawer anchor='right'
+                        open={toggleFlag}
+                        onClose={toggleDrawer}>
                         <div className={classes.sideBar}>
-                            <MenuList sideBarFlag = {true}/>
+                            <MenuList sideBarFlag={true} />
                         </div>
                     </Drawer>
                 </Toolbar>
--- frontend/src/page/SignUp/SignUp.js
+++ frontend/src/page/SignUp/SignUp.js
@@ -1,14 +1,45 @@
 import React, { useState } from "react";
-import Input from "@material-ui/core/Input";
-import FormLabel from "@material-ui/core/FormLabel";
 import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 import axios from 'axios';
+{/* CSS부분 */ }
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 const SignUp = () => {
     const [user_name, setName] = useState(null);
-    const [user_Id, setuser_Id] = useState(null);
-    const [password, setPass] = useState(null);
-    const [passCheck, setPassCheck] = useState(null);
+    const [user_Id, setuser_Id] = useState('');
+    const [password, setPass] = useState('');
+    const [passCheck, setPassCheck] = useState('');
+
+    {/* MODAL 부분 */}
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    {/* MODAL 부분 */}
+
     var checkPass = new Boolean(false);
     var checkId = new Boolean(true);
 
@@ -24,11 +55,8 @@ const SignUp = () => {
     const setPassWordCheck = e => {
         setPassCheck(e.target.value);
     };
-    const save = e => {
-        e.preventDefault();
-    };
 
-
+    {/* 회원가입 버튼을 눌렀을때 실행될 함수 */ }
     const add_User = () => {
         if (checkPass == true && checkId == true) {
             axios.post('/api/user/addUser', {
@@ -49,11 +77,12 @@ const SignUp = () => {
         }
     }
 
+    {/* 아이디 중복체크를 눌렀을때 실행될 함수 */ }
     const checkuser_Id = () => {
-        var user = document.getElementById("user_Id").value;
-        if (user.length === 0) {
+        if (user_Id === '') {
             alert("아이디(학번)를 입력해주세요.");
         } else {
+
             axios.post('/api/user/checkuser_Id', {
                 user_Id: user_Id,
             })
@@ -74,13 +103,11 @@ const SignUp = () => {
         }
     }
 
+    {/* 비밀번호확인을 눌렀을때 실행될 함수 */ }
     const passWordCheck = () => {
-        var pass1 = document.getElementById("password").value;
-        var pass2 = document.getElementById("passCheck").value;
-
-        if (pass1 === '' || pass2 === '') {
+        if (password === '' || passCheck === '') {
             alert("비밀번호를 입력해주세요.");
-        } else if (pass1 !== pass2) {
+        } else if (password !== passCheck) {
             alert("비밀번호가 틀립니다. 다시 입력해주세요.");
         } else {
             alert("비밀번호가 맞습니다.");
@@ -89,51 +116,80 @@ const SignUp = () => {
     }
 
 
-
     return (
-        <div className="SignUp" id="signup">
-            <form onSubmit={save}>
-                <FormLabel htmlFor="user_name">이름</FormLabel>
-                <Input
-                    type="text"
-                    name="user_name"
-                    id="user_name"
-                    onChange={setNameText}
-                />
-                <br />
+        <div className="div" id="signup">
+            <Input
+                className="input"
+                placeholder="이름"
+                type="text"
+                name="user_name"
+                id="user_name"
+                onChange={setNameText}
+            />
+            <br />
 
-                <FormLabel htmlFor="user_Id">아이디(학번)</FormLabel>
-                <Input
-                    type="number"
-                    name="user_Id"
-                    id="user_Id"
-                    onChange={setIdText}
-                />
-                <Button value="중복체크" onClick={checkuser_Id}>중복체크</Button>
-                <br />
+            <Input
+                className="input"
+                placeholder="아이디(학번)"
+                type="number"
+                name="user_Id"
+                id="user_Id"
+                onChange={setIdText}
+            />
 
-                <FormLabel htmlFor="password">비밀번호</FormLabel>
-                <Input
-                    type="password"
-                    name="password"
-                    id="password"
-                    onChange={setPassWord}
-                />
-                <br />
+            <Button className="button1" variant="outlined" color="primary" value="중복체크" onClick={checkuser_Id}>아이디 중복확인</Button>
+            <br />
 
-                <FormLabel htmlFor="passCheck">비밀번호확인</FormLabel>
-                <Input
-                    type="password"
-                    name="passCheck"
-                    id="passCheck"
-                    onChange={setPassWordCheck}
-                />
-                <Button value="비밀번호확인" onClick={passWordCheck}>비밀번호확인</Button>
-                <br />
+            <Input
+                className="input"
+                placeholder="비밀번호"
+                type="password"
+                name="password"
+                id="password"
+                onChange={setPassWord}
+            />
+            <br />
 
-                <Button value="가입완료" onClick={add_User}>가입완료</Button>
-                {/*가입완료 누를시 로그인 창으로 들어감(onClick구현)*/}
-            </form>
+            <Input
+                className="input"
+                placeholder="비밀번호 재확인"
+                type="password"
+                name="passCheck"
+                id="passCheck"
+                onChange={setPassWordCheck}
+            />
+            <Button className="button1" variant="outlined" color="primary" value="비밀번호 재확인" onClick={passWordCheck}>비밀번호 재확인</Button>
+            <br />
+
+            <Button className="button2" variant="outlined" color="primary" value="가입완료" onClick={add_User}>가입완료</Button>
+            <br />
+
+
+            {/* MODAL 부분 */}
+            <button type="button" onClick={handleOpen}>
+                modal check!
+            </button>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <div className={classes.paper}>
+                        <SignUp/>
+                    </div>
+                </Fade>
+            </Modal>
+            {/* MODAL 부분 */}
+
+
         </div>
     );
 };
