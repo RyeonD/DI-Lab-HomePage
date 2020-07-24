48ba8f584fe22960ed0c9602d49242ed34a697ac
solo95 <wengers1004@naver.com>
2020-07-24 22:29:29 +0900

    sample Config 추가

---
+++ backend/config/dbConfigSample.js
@@ -0,0 +1,9 @@
+const host = 'example 127.0.0.0'
+module.exports ={
+    mysql:{
+        user     : 'example',
+        password : 'example',
+        host     : host,
+        database : 'example',
+    }
+}
