aedba367808acca98452ada53abd3d2ea5ad3be3
RyeonDeveloper <luvlik207@gmail.com>
2020-07-25 01:39:43 +0900

    Member Page UI 변경 중

--- frontend/src/page/Member/Member.js
+++ frontend/src/page/Member/Member.js
@@ -1,36 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { Box, Avatar, Typography, Link, AppBar, Tabs, Tab } from '@material-ui/core';
+import React, { useState } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+import { makeStyles, } from '@material-ui/core/styles';
+import { Box, Avatar, Typography, Link, } from '@material-ui/core';
 
 import img from './cat.jpg'
 import shadows from '@material-ui/core/styles/shadows';
 
 const useStyles = makeStyles((theme) => ({
-    '@media screen and (max-width: 900px)': {
+    '@media screen and (max-width: 1000px)': {
         rankBox: {
-            
+
         },
         rankTitle: {
-            margin: '3vw auto',
-            width: '40vw',
+            margin: '5%',
+            width: '20vw',
         },
         rankTitleEng: {
-            fontSize: '4vw',
+            fontSize: '5vw',
         },
         rankTitleKor: {
             fontSize: '3vw'
         },
-        profileBox: { 
-            width: '20vw',
-            margin: 'auto', 
+        profileBox: {
+            width: '70vw',
+            margin: 'auto',
         },
         profile: {
-            width: '40vw',
-            margin: '3vw auto',
+            width: '25vw',
+            margin: '5vw auto',
         },
         profileImg: {
-            width: '13vw',
-            height: '13vw',
+            width: '15vw',
+            height: '15vw',
             margin: '10px auto'
         },
         infoName: {
@@ -40,71 +42,71 @@ const useStyles = makeStyles((theme) => ({
             fontSize: '2.2vw'
         }
     },
-    '@media screen and (min-width: 901px)': {
+    '@media screen and (min-width: 1001px)': {
         rankBox: {
-            height: '50vw',
+
         },
         rankTitle: {
-            width: '250px',
+            width: '500px',
         },
         rankTitleEng: {
-            fontSize: '37px', 
+            fontSize: '40px',
         },
         rankTitleKor: {
             fontSize: '20px',
-            color: '#82848c',
         },
-        profileBox: { 
-            width: '800px',
+        profileBox: {
+            width: '1000px',
+            padding: '70px'
         },
         profile: {
-            width: '200px',
-            margin: '25px 3px',
+            width: '300px',
+            margin: '50px 5px',
         },
         profileImg: {
-            width: theme.spacing(20),
-            height: theme.spacing(20),
+            width: theme.spacing(27),
+            height: theme.spacing(27),
             margin: '10px auto'
         },
         infoName: {
-            fontSize: '27px',
+            fontSize: '28px',
+            margin: '4px'
         },
         info: {
-            fontSize: '16px'
+            fontSize: '18px',
+            margin: '4px',
         }
     },
     root: {
-        
-    },
-    memberBox: {
         display: 'flex',
+        alignItems: 'center',
         flexDirection: 'column',
     },
     rankBox: {
+        width: '100vw',
         display: 'flex',
         flexDirection: 'row',
-        justifyItems: 'center',
+        justifyContent: 'space-around',
         alignItems: 'center',
     },
     rankTitleEng: {
         color: '#414352',
-        fontWeight:'bold',
+        fontWeight: 'bold',
         textTransform: 'uppercase',
         textAlign: 'center',
     },
     rankTitleKor: {
-        fontWeight:'bold',
+        fontWeight: 'bold',
         textAlign: 'center',
+        color: '#82848c',
     },
     profileBox: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-around',
         flexWrap: 'wrap',
-        backgroundColor: '#d6d6ff'
     },
     profile: {
-
     },
     profileImg: {
 
@@ -115,150 +117,187 @@ const useStyles = makeStyles((theme) => ({
     },
     info: {
         textAlign: 'center',
+        color: '#82848c',
     },
     infoHome: {
-
     }
 }));
 
 const rankList = [
-    { eng: 'professor',
-      kor: '교수',
+    {
+        eng: 'professor',
+        kor: '교수',
     },
-    { eng: 'doctor',
-      kor: '박사',
+    {
+        eng: 'doctor',
+        kor: '박사',
     },
-    { eng: 'master',
-      kor: '석사',
+    {
+        eng: 'master',
+        kor: '석사',
     },
-    { eng: 'student',
-      kor: '학사',
+    {
+        eng: 'bachelor',
+        kor: '학사',
     },
-    { eng: 'alumni',
-      kor: '졸업생',
+    {
+        eng: 'alumni',
+        kor: '졸업생',
     },
 ];
 const memberInfoMenu = [
-    'rank','name','email'
+    'rank', 'name', 'email'
 ];
 const memberList = [
-    {   rank: 'professor',
+    {
+        rank: 'professor',
         name: '김화종',
         image: img,
-        email: 'rytnsla@kangwon.ac.kr',
-        room: '4호',
+        email: 'hjkim@gmail.com',
+        room: '공대 6호관 403호',
         homepage: 'https://www.kangwon.ac.kr/www/index.do'
     },
-    {   rank: 'doctor',
+    {
+        rank: 'doctor',
         name: '김종원',
         image: '',
-        email: '123@kangwon.ac.kr' ,
+        email: '123@kangwon.ac.kr',
         homepage: 'df'
     },
-    {   rank: 'master',
+    {
+        rank: 'master',
         name: '김경',
         image: '',
-        email: '123@kangwon.ac.kr' 
+        email: '123@kangwon.ac.kr'
     },
-    {   rank: 'doctor',
+    {
+        rank: 'doctor',
         name: '김련경',
         image: '',
         email: '12@kangwon.ac.kr'
     },
-    {   rank: 'master',
+    {
+        rank: 'master',
         name: '김욱',
         image: '',
         email: '12@kangwon.ac.kr'
     },
-    {   rank: 'student',
+    {
+        rank: 'bachelor',
         name: '김원',
         image: '',
-        email: '123@kangwon.ac.kr' 
+        email: '123@kangwon.ac.kr'
     },
-    {   rank: 'student',
+    {
+        rank: 'bachelor',
         name: '김련',
         image: '',
         email: '12@kangwon.ac.kr'
     },
-    {   rank: 'student',
+    {
+        rank: 'bachelor',
         name: '김종원',
         image: '',
-        email: '123@kangwon.ac.kr' 
+        email: '123@kangwon.ac.kr'
     },
-    {   rank: 'student',
+    {
+        rank: 'bachelor',
         name: '김경',
         image: '',
-        email: '123@kangwon.ac.kr' 
+        email: '123@kangwon.ac.kr'
     },
-    {   rank: 'student',
+    {
+        rank: 'bachelor',
         name: '김련경',
         image: '',
         email: '12@kangwon.ac.kr'
     },
-    {   rank: 'student',
+    {
+        rank: 'bachelor',
         name: '김욱',
         image: '',
         email: '12@kangwon.ac.kr'
     },
-    {   rank: 'alumni',
+    {
+        rank: 'alumni',
         name: '김종',
         image: '',
         email: '123@kangwon.ac.kr'
     },
-    {   rank: 'alumni',
+    {
+        rank: 'alumni',
         name: '김선',
         image: '',
         email: '',
         job: '네이버'
     },
-]  
+    {
+        rank: 'bachelor',
+        name: '김련',
+        image: '',
+        email: '12@kangwon.ac.kr'
+    },
+    {
+        rank: 'bachelor',
+        name: '김종원',
+        image: '',
+        email: '123@kangwon.ac.kr'
+    },
+    {
+        rank: 'bachelor',
+        name: '김경',
+        image: '',
+        email: '123@kangwon.ac.kr'
+    },
+]
 
 const Member = () => {
     const classes = useStyles();
-    const [value, setValue] = useState('');
 
-    const handleChange = () => {
-        setValue('red');
-    }
+    AOS.init();
 
     return (
-        <div className={classes.root}>
-        <div className={classes.memberBox}>
-            {rankList.map((rank) =>
-                <Box    className={classes.rankBox}
-                        id={rank.eng}>
+        <div className={classes.root} >
+            {rankList.map((rank, i) =>
+                <div className={classes.rankBox}
+                    key={i}
+                    id={rank.eng}
+                    data-aos="fade-up"
+                    data-aos-item="fade-left"
+                    style={{ backgroundColor: i % 2 === 0 ? '#e3e3ff' : 'white' }}>
                     <Box className={classes.rankTitle}>
                         <Typography className={classes.rankTitleEng}>{rank.eng}</Typography>
                         <Typography className={classes.rankTitleKor}>{rank.kor}</Typography>
                     </Box>
                     <Box className={classes.profileBox}>
-                    { memberList.map((member) => {
-                        if(member.rank === rank.eng) {
-                            return (
-                                <div className={classes.profile}>
-                                    <Avatar className={classes.profileImg}
+                        {memberList.map((member, j) => {
+                            if (member.rank === rank.eng) {
+                                return (
+                                    <div className={classes.profile}
+                                        key={j}>
+                                        <Avatar className={classes.profileImg}
                                             src={member.image}></Avatar>
-                                    <Typography className={classes.infoName}>{member.name}</Typography>
-                                    <Typography className={classes.info}>{member.job}</Typography>
-                                    <Typography className={classes.info}>{member.room}</Typography>
-                                    <Typography className={classes.info}>{member.email}</Typography>
-                                    <Typography className={classes.info}>
-                                        {member.homepage ? 
-                                            <Link   className={classes.infoHome}
-                                                href={member.homepage}
-                                                target="_blank"
-                                                underline="always">
-                                            홈페이지
+                                        <Typography className={classes.infoName}>{member.name}</Typography>
+                                        <Typography className={classes.info}>{member.job}</Typography>
+                                        <Typography className={classes.info}>{member.room}</Typography>
+                                        <Typography className={classes.info}>{member.email}</Typography>
+                                        <Typography className={classes.info}>
+                                            {member.homepage ?
+                                                <Link className={classes.infoHome}
+                                                    href={member.homepage}
+                                                    target="_blank"
+                                                    underline="hover">
+                                                    홈페이지
                                         </Link> : null}
-                                    </Typography>
-                                </div>
-                            )
-                        }
-                    })}
+                                        </Typography>
+                                    </div>
+                                )
+                            }
+                        })}
                     </Box>
-                </Box>
+                </div>
+
             )}
-        </div>
         </div>
     )
 }
