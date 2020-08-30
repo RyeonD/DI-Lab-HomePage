42b82120b62f17d0c68654ab09d914af799bd593
RyeonDeveloper <luvlik207@gmail.com>
2020-08-30 22:09:19 +0900

    member page UI 변경

--- frontend/src/page/Member/Member.js
+++ frontend/src/page/Member/Member.js
@@ -3,8 +3,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'
 import { makeStyles, } from '@material-ui/core/styles';
 import { Box, Avatar, Typography, Link, } from '@material-ui/core';
-
-import img from './cat.jpg'
+import SvgIcon from '@material-ui/core/SvgIcon';
 
 const useStyles = makeStyles((theme) => ({
     '@media screen and (max-width: 1000px)': {
@@ -12,39 +11,55 @@ const useStyles = makeStyles((theme) => ({
             
         },
         rankTitle: {
-            margin: '5%',
+            margin: '3vw',
         },
         rankTitleEng: {
-            fontSize: '5vw',
+            fontSize: '4vw',
         },
         rankTitleKor: {
-            fontSize: '3vw'
+            fontSize: '2vw'
         },
         profileBox: {
-            width: '70vw',
-            margin: 'auto',
+            width: '90vw',
         },
         profile: {
-            width: '25vw',
-            margin: '5vw auto',
+            width: '37vw',
+            margin: '5vw 3vw',
+            padding: '5vw 1vw',
+        },
+        infomation: {
+            width: '20vw',
+            margin: '0 2vw',
+            
+        },
+        alumniProfile: { 
+            width: '20vw',
+            margin: '3vw 3vw',
         },
         profileImg: {
-            width: '15vw',
-            height: '15vw',
-            margin: '10px auto'
+            width: '10vw',
+            height: '10vw',
         },
         infoName: {
-            fontSize: '3vw',
+            fontSize: '2.2vw',
         },
         info: {
-            fontSize: '2vw'
-        }
+            fontSize: '1.2vw'
+        },
+        homepage: {
+            fontSize: '1.9vw'
+        },
     },
     '@media screen and (min-width: 1001px)': {
+        root: {
+            margin: '0 50px'
+        },
         rankBox: {
         },
         rankTitle: {
             width: '500px',
+            margin: '40px 0 0',
+            padding: '5px'
         },
         rankTitleEng: {
             fontSize: '40px',
@@ -53,37 +68,43 @@ const useStyles = makeStyles((theme) => ({
             fontSize: '20px',
         },
         profileBox: {
-            width: '1700px',
-            padding: '70px',
+            width: '1200px',
+            padding: '0 50px',
         },
         profile: {
-            width: '300px',
+            width: '470px',
             margin: '50px 5px',
+        },
+        alumniProfile: { 
+            width: '300px',
+            margin: '40px 10px',
         },
         profileImg: {
             width: theme.spacing(20),
             height: theme.spacing(20),
-            margin: '10px auto'
+        },
+        infomation: {
+            margin: '0 10px',
         },
         infoName: {
             fontSize: '28px',
-            margin: '4px'
+            padding: '5px 0'
         },
         info: {
-            fontSize: '18px',
-            margin: '4px',
+            fontSize: '16px',
+        },
+        homepage: {
+            fontSize: '21.5px'
         }
     },
     root: {
         display: 'flex',
         alignItems: 'center',
         flexDirection: 'column',
-        margin:'0 12vw'
     },
     rankBox: {
-        width: '100%',
         display: 'flex',
-        flexDirection: 'row',
+        flexDirection: 'column',
         justifyContent: 'space-around',
         alignItems: 'center',
     },
@@ -101,24 +122,30 @@ const useStyles = makeStyles((theme) => ({
     profileBox: {
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-around',
+        justifyContent: 'space-between',
         flexWrap: 'wrap',
     },
     profile: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    alumniProfile: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+    },
+    infomation: {
+        display: 'flex',
+        flexDirection: 'column',
     },
     profileImg: {
-
     },
     infoName: {
-        textAlign: 'center',
-        margin: '0 0 1vw'
     },
     info: {
-        textAlign: 'center',
         color: '#82848c',
     },
-    infoHome: {
-    }
 }));
 
 const rankList = [
@@ -152,8 +179,9 @@ const memberList = [
         rank: 'doctor',
         name: '홍성은',
         image: '',
+        interest: '시퀀스 데이터 분석 / 임베딩 / 딥러닝',
         email: '@kangwon.ac.kr',
-        homepage: 'httsp://github.com/sungkenh',
+        homepage: 'https://github.com/sungkenh',
     },
     {
         rank: 'master',
@@ -174,12 +202,6 @@ const memberList = [
         email: '@kangwon.ac.kr'
     },
     {
-        rank: 'master',
-        name: '이현수',
-        image: '',
-        email: '@kangwon.ac.kr'
-    },
-    {
         rank: 'bachelor',
         name: '김종원',
         image: '',
@@ -195,7 +217,7 @@ const memberList = [
         rank: 'alumni',
         name: '권혁민',
         image: '',
-        email: '123@kangwon.ac.kr',
+        email: '',
         job: '스위트케이',
     },
     {
@@ -239,6 +261,7 @@ const memberList = [
         image: '',
         email: '',
         job: '',
+        degree: 'M.S. degree',
     },
     {
         rank: 'alumni',
@@ -246,6 +269,7 @@ const memberList = [
         image: '',
         email: '',
         job: '더존',
+        degree: 'M.S. degree',
     },
     {
         rank: 'alumni',
@@ -274,6 +298,7 @@ const memberList = [
         image: '',
         email: '',
         job: '카카오',
+        degree: 'M.S. degree',
     },
     {
         rank: 'alumni',
@@ -286,7 +311,6 @@ const memberList = [
 
 const Member = () => {
     const classes = useStyles();
-
     AOS.init();
 
     return (
@@ -296,9 +320,9 @@ const Member = () => {
                     key={i}
                     id={rank.eng}
                     data-aos="fade-up"
-                    data-aos-item="fade-left"
-                    style={{ backgroundColor: i % 2 === 0 ? '#e3e3ff' : 'white' }}>
-                    <Box className={classes.rankTitle}>
+                    style={{ backgroundColor: i % 2 === 0 ? '' : '#f2f7ff' }}>
+                    <Box className={classes.rankTitle}
+                        data-aos="fade-up">
                         <Typography className={classes.rankTitleEng}>{rank.eng}</Typography>
                         <Typography className={classes.rankTitleKor}>{rank.kor}</Typography>
                     </Box>
@@ -306,23 +330,30 @@ const Member = () => {
                         {memberList.map((member, j) => {
                             if (member.rank === rank.eng) {
                                 return (
-                                    <div className={classes.profile}
-                                        key={j}>
+                                    <div className={rank.eng === 'alumni' ? classes.alumniProfile : classes.profile}
+                                        key={j}
+                                        data-aos="fade-up">
                                         <Avatar className={classes.profileImg}
                                             src={member.image}></Avatar>
-                                        <Typography className={classes.infoName}>{member.name}</Typography>
-                                        <Typography className={classes.info}>{member.job}</Typography>
-                                        <Typography className={classes.info}>{member.room}</Typography>
-                                        <Typography className={classes.info}>{member.email}</Typography>
-                                        <Typography className={classes.info}>
-                                            {member.homepage ?
-                                                <Link className={classes.infoHome}
-                                                    href={member.homepage}
-                                                    target="_blank"
-                                                    underline="hover">
-                                                    홈페이지
-                                        </Link> : null}
-                                        </Typography>
+                                        <div className={classes.infomation}>
+                                            {member.rank === 'alumni' ?
+                                                <Typography className={classes.info}>{member.degree ? member.degree : <br></br>}</Typography>
+                                                : ''}
+                                            <Typography className={classes.infoName}>
+                                                {member.name} {member.homepage ?
+                                                    <Link href={member.homepage}
+                                                        target="_blank"
+                                                        underline="hover">
+                                                        <SvgIcon className={classes.homepage}>
+                                                            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+                                                        </SvgIcon>
+                                                    </Link> : null}
+                                            </Typography>
+                                            <Typography className={classes.info}>{member.job}</Typography>
+                                            <Typography className={classes.info}>{member.interest}</Typography>
+                                            <Typography className={classes.info}>{member.email}</Typography>
+                                            
+                                        </div>
                                     </div>
                                 )
                             }
